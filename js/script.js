a96c552ff89ebccdd9be0835d34f85920cfbbb9d
rahulbshrestha <ra.shrestha@jacobs-university.de>
2019-05-05 19:47:00 +0200

    +text box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var numberofCells = 300;
+  var numberofCells = 100;
   var cell;
   var h;
 
@@ -8,31 +8,32 @@ window.onload = function() {
   for (var i = 1; i <= numberofCells; i++) {
     if (i == 1) { //Blank space left for 1
       cell = $('<div>').addClass('cell').attr('data-cell', i).text("");
-    }else{
+    } else {
       cell = $('<div>').addClass('cell').attr('data-cell', i).text(i);
     }
 
     for (var j = 1; j <= numberofCells; j++) {
-      if (a[j] == i) { //Check if number fal;s under array of prime numbers
+      if (a[j] == i) { //Check if number falls under array of prime numbers
         cell.addClass('prime');
+        $("#textBox").addClass('text').append(a[j] + " . " );
       }
     }
-
-    if (i % 2 == 0) {
-      cell.addClass('multipleof2');
-    }
-    if (i % 3 == 0) {
-      cell.addClass('multipleof3');
-    }
-    if (i % 4 == 0) {
-      cell.addClass('multipleof4');
-    }
-    if (i % 5 == 0){
-      cell.addClass('multipleof5');
-    }
-    if (i % 7 == 0){
-      cell.addClass('multipleof7');
-    }
+    //
+    // if (i % 2 == 0) {
+    //   cell.addClass('multipleof2');
+    // }
+    // if (i % 3 == 0) {
+    //   cell.addClass('multipleof3');
+    // }
+    // if (i % 4 == 0) {
+    //   cell.addClass('multipleof4');
+    // }
+    // if (i % 5 == 0) {
+    //   cell.addClass('multipleof5');
+    // }
+    // if (i % 7 == 0) {
+    //   cell.addClass('multipleof7');
+    // }
     $('#game').append(cell);
   }
 
@@ -41,6 +42,8 @@ window.onload = function() {
     height: h,
     lineHeight: h + 'px'
   });
+
+
 }
 
 function getPrimes(num) {
@@ -48,9 +51,11 @@ function getPrimes(num) {
   arr[1] = 0; //Removing 1 from being a prime number
   var rt = Math.sqrt(num); // calculate only once
   for (i = 2; i <= rt; i++) {
-    for (var j = i * i; j <= num; j += i) {
-      arr[j] = 0; //Replacing non prime numbers with 0
+    if (arr[i]) {
+      for (var j = i * i; j <= num; j += i) {
+        arr[j] = 0; //Replacing non prime numbers with 0
+      }
     }
   }
-  return arr.filter(Number); // kick out the zeroes
+  return arr;
 }
