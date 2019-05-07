5d1578784e6db76d05f7504fe1941c575b4b64da
rahulbshrestha <ra.shrestha@jacobs-university.de>
2019-05-07 21:40:25 +0200

    +input box

--- js/script.js
+++ js/script.js
@@ -1,61 +1,56 @@
-window.onload = function() {
-  var numberofCells = 100;
+$(document).ready(function() {
+  var numberofCells;
   var cell;
   var h;
 
-  var a = getPrimes(numberofCells);
+  $('#primeHeader').hide();
 
-  for (var i = 1; i <= numberofCells; i++) {
-    if (i == 1) { //Blank space left for 1
-      cell = $('<div>').addClass('cell').attr('data-cell', i).text("");
-    } else {
-      cell = $('<div>').addClass('cell').attr('data-cell', i).text(i);
-    }
+  var addValueButton = document.getElementById('testid');
 
-    for (var j = 1; j <= numberofCells; j++) {
-      if (a[j] == i) { //Check if number falls under array of prime numbers
-        cell.addClass('prime');
-        $("#textBox").addClass('text').append(a[j] + " . " );
+  addValueButton.addEventListener('click', function(e) {
+
+    $('#primeHeader').show();
+    $('#inputBox').hide();
+    var inputtext = jQuery("#input").val();
+    inputtext = parseInt(inputtext);
+
+    var numberofCells = inputtext;
+    var a = getPrimes(numberofCells);
+
+    for (var i = 1; i <= numberofCells; i++) { //Add cells
+      if (i == 1) { //Blank space left for 1
+        cell = $('<div>').addClass('cell').attr('data-cell', i).text("");
+      } else {
+        cell = $('<div>').addClass('cell').attr('data-cell', i).text(i);
       }
+
+      for (var j = 1; j <= numberofCells; j++) {
+        if (a[j] == i) { //Check if current number is prime and color if it is
+          cell.addClass('prime');
+          $("#textBox").addClass('text').append(a[j] + "&nbsp; &nbsp; ");
+        }
+      }
+      $('#numberchart').append(cell);
     }
-    //
-    // if (i % 2 == 0) {
-    //   cell.addClass('multipleof2');
-    // }
-    // if (i % 3 == 0) {
-    //   cell.addClass('multipleof3');
-    // }
-    // if (i % 4 == 0) {
-    //   cell.addClass('multipleof4');
-    // }
-    // if (i % 5 == 0) {
-    //   cell.addClass('multipleof5');
-    // }
-    // if (i % 7 == 0) {
-    //   cell.addClass('multipleof7');
-    // }
-    $('#game').append(cell);
-  }
 
-  h = $('.cell:last-of-type').width();
-  $('.cell').css({
-    height: h,
-    lineHeight: h + 'px'
+    h = $('.cell:last-of-type').width();
+    $('.cell').css({
+      height: h,
+      lineHeight: h + 'px'
+    });
   });
+});
 
-
-}
-
-function getPrimes(num) {
-  var arr = [...Array(num + 1).keys()]; // =[0,1,...,num]
+function getPrimes(n) {
+  var arr = [...Array(n + 1).keys()]; //arr contains elements from 0 to n
   arr[1] = 0; //Removing 1 from being a prime number
-  var rt = Math.sqrt(num); // calculate only once
+  var rt = Math.sqrt(n); // calculate only once
   for (i = 2; i <= rt; i++) {
     if (arr[i]) {
-      for (var j = i * i; j <= num; j += i) {
+      for (var j = i * i; j <= n; j += i) {
         arr[j] = 0; //Replacing non prime numbers with 0
       }
     }
   }
-  return arr;
+  return arr.filter(Number); //Removing 0's from array
 }
